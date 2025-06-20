53f04607b3a51968569a1de6cc5e7b4dcbe12ee3
Redietkifle <redietkifle14@gmail.com>
2025-06-20 18:36:54 +0300

    Update miniGit.hpp

--- miniGit.hpp
+++ miniGit.hpp
@@ -1,1 +1,12 @@
 //miniGit.hpp
+#ifndef MINIGIT_HPP
+#define MINIGIT_HPP
+
+#include <string>
+#include <unordered_map>
+std::string readHEAD(); 
+void writeHEAD(const std::string& branch); 
+std::string readBranch(const std::string& branch); 
+void writeBranch(const std::string& branch, const std::string& 
+commitHash); 
+void loadBranches(); 
